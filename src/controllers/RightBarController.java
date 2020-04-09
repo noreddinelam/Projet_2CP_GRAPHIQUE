ab4e2ea76ea42b72345decf1713343bcac2f400a
boussekine <ia_boussekine@esi.dz>
2020-04-09 15:44:35 +0200

    adding the Chronogramme and TableDeVerite files and puting them in place + working with the two buttoms nouveau and fermer

--- src/controllers/RightBarController.java
+++ src/controllers/RightBarController.java
@@ -8,17 +8,21 @@ import java.net.URL;
 import java.util.ResourceBundle;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 public class RightBarController implements Initializable {
 	
 	
 	
-	
+	@FXML
+    private Button fermer;
 	
 	@Override
 	public void initialize(URL url, ResourceBundle rb) {
@@ -65,16 +69,76 @@ public class RightBarController implements Initializable {
 		        stage.setScene(scene);  
 		        stage.setTitle("About SimulINI");
 		        stage.setResizable(false);
+			    stage.initModality(Modality.APPLICATION_MODAL);
+
+		        stage.show();
+		       //stage.showAndWait();
+		    } catch(Exception e) {
+		        e.printStackTrace();
+		    }
+
+		
+	}
+	
+	
+	public void tableDeVerite(ActionEvent event) {
+		 try {
+		        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/application/TableDeVerite.fxml"));
+		        Parent root = fxmlLoader.load();
+		        Stage stage = new Stage();
+		        Scene scene = new Scene(root);
+		        stage.setScene(scene);  
+		        stage.setTitle("la table de verité");
+		        stage.setResizable(false);
+			    stage.initModality(Modality.APPLICATION_MODAL);
+
+		        stage.show();
+		    } catch(Exception e) {
+		        e.printStackTrace();
+		    	}
+	}
+	
+	
+	public void chronogramme(ActionEvent event) {
+		 try {
+		        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/application/Chronogramme.fxml"));
+		        Parent root = fxmlLoader.load();
+		        Stage stage = new Stage();
+		        Scene scene = new Scene(root);
+		        stage.setScene(scene);  
+		        stage.setTitle("le chronogramme");
+		        stage.setResizable(false);
+			    stage.initModality(Modality.APPLICATION_MODAL);
+
+		        stage.show();
+		    } catch(Exception e) {
+		        e.printStackTrace();
+		    	}
+	}
+	
+	
+	public void fermer(ActionEvent event) {
+		Stage stage = (Stage) fermer.getScene().getWindow();
+		stage.close();
+	}
+	
+	public void nouveau(ActionEvent event) {
+		
+		 try {
+		        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/application/Home.fxml"));
+		        Parent root = fxmlLoader.load();
+		        Stage stage = new Stage();
+		        Scene scene = new Scene(root);
+		        stage.setScene(scene);  
+		        stage.setResizable(false);
+
 		        stage.show();
 		    } catch(Exception e) {
 		        e.printStackTrace();
 		    }
-		
-
-		
-	
-		
+		 
 	}
+
 	
 	
 	
